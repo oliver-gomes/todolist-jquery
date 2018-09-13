9c574f2a58149584a92a3341b967c6d436884acf
Oliver-Gomes <ogomesphoto@gmail.com>
2018-09-13 03:22:59 -0400

    removing todo when clicked X

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,3 +1,10 @@
+//Check off specific todos by clicking
 $("li").on("click", function () {
   $(this).toggleClass('completed')
 });
+
+//click on X to delete
+$('span').click(function () {
+  $(this).parent().remove();
+  event.stopPropagation()
+})
