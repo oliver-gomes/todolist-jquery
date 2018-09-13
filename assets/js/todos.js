49a76137f1923855e0b7092ad446ff034a359188
Oliver-Gomes <ogomesphoto@gmail.com>
2018-09-13 03:03:42 -0400

    get rid of control flow and use toggleClass

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,18 +1,3 @@
 $("li").on("click", function () {
-
-  //if li is gray
-  if ($(this).css('color') === 'rgb(128, 128, 128)') {
-    //turn it black
-    $(this).css({
-      color: "black",
-      textDecoration: "none"
-    });
-  }
-  else {
-    //turn it gray
-    $(this).css({
-      color: "gray",
-      textDecoration: "line-through"
-    });
-  }
+  $(this).toggleClass('completed')
 });
