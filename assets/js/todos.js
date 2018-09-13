bf7b0a90d0bad83e89fa7fbaf8a94e7ebd29dda8
Oliver-Gomes <ogomesphoto@gmail.com>
2018-09-13 03:00:55 -0400

    control flow for click and strikethrough

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,6 +1,18 @@
 $("li").on("click", function () {
-  $(this).css({
-    color: "gray",
-    textDecoration: "line-through"
-  });
+
+  //if li is gray
+  if ($(this).css('color') === 'rgb(128, 128, 128)') {
+    //turn it black
+    $(this).css({
+      color: "black",
+      textDecoration: "none"
+    });
+  }
+  else {
+    //turn it gray
+    $(this).css({
+      color: "gray",
+      textDecoration: "line-through"
+    });
+  }
 });
