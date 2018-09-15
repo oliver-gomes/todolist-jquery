00ec668bbb9e5d0dde2ca37aef365428ee4dc62c
Oliver-Gomes <ogomesphoto@gmail.com>
2018-09-15 14:19:06 -0400

    new version updated

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,23 +1,26 @@
 //Check off specific todos by clicking
-$("ul").on("click", 'li', function () {
-  $(this).toggleClass('completed');
+$("ul").on("click", '.p', function () {
+  $(this).parent().toggleClass('completed');
 });
 
 //click on X to delete
-$('ul').on('click', 'span', function () {
-  $(this).parent().fadeOut(500, function () {
+$('ul').on('click', '.fa', function (event) {
+  $(this).parent().parent().fadeOut(500, function () {
     $(this).remove();
   })
   event.stopPropagation();
 });
 
-$("input[type='text']").keypress(function (event) {
+$("input[type='text']").on("keypress", function (event) {
   if (event.which === 13) {
     //grabbing new todo text from input
     var todoText = $(this).val();
     $(this).val("");
     //create a new li and add ul
-    $('ul').append(`<li><span><i class="fa fa-trash"></i></span>${todoText}</li>`);
+    $('ul').append(`<li>
+    <span class="p">${todoText}</span>
+    <span><i class="fa fa-trash-alt"></i></span>
+  </li>`);
   }
 })
 
